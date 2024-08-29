bd5149edcee831ae2d10fd047a311395812ead70
robyafrizal <robyafrizal86@gmail.com>
2024-08-29 14:04:39 +0700

    create first template with react and tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './index.css'
+// import 'antd/dist/reset.css';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import HomePage from './pages/HomePage'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path='/' element={<HomePage/>}/>
+//           <Route path='/login' element={<Login/>}/>
+//           <Route path='/register' element={<Register/>}/>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
 
+// export default App
+
+import React from 'react';
+import './index.css';
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className='text-red-800'>App</h1>
+    </div>
   )
 }
-
-export default App
