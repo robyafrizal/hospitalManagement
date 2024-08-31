3e6aa8a49510bd6a970bc2d18f195ebacb52cd9e
robyafrizal <robyafrizal86@gmail.com>
2024-09-01 00:42:08 +0700

    set loading and set error in register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,25 +2,39 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Register() {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value})
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   // console.log(formData)
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    //CORS policy - setup from vite.config and remove URL http://localhost:8080
-    const res = await fetch("http://localhost:8080/api/auth/register",{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-    const data = await res.json()
-    console.log(data)
-  }
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false)
+      //CORS policy - setup from vite.config and remove URL http://localhost:8080
+      const res = await fetch("http://localhost:8080/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      // console.log(data)
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="max-w-lg mx-auto">
       <div className="text-3xl text-center font-semibold my-7">Register</div>
@@ -46,8 +60,11 @@ export default function Register() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-95 disabled:opacity-80">
-          Register
+        <button
+          disabled={loading}
+          className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Register"}
         </button>
       </form>
       <div className="flex gap-2 mt-3">
@@ -56,6 +73,7 @@ export default function Register() {
           <span className="text-blue-500">Login</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-3">{error && "Something went wrong"}</p>
     </div>
   );
 }
