5097f4b69bffe6059df6d26209366e004e88d67b
robyafrizal <robyafrizal86@gmail.com>
2024-09-02 05:37:25 +0700

    Setup Login Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
 const errorHandler = require("../middlewares/errorHandler");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const authController = async (req, res, next) => {
+const register = async (req, res, next) => {
   // console.log(req.body);
   // const payload = req.body;\
 
@@ -19,4 +20,23 @@ const authController = async (req, res, next) => {
   }
 };
 
-module.exports = authController;
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...userWithoutPassword } = validUser._doc;
+    const expireDate = new Date(Date.now() + 3600000); //1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expireDate })
+      .status(200)
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login };
