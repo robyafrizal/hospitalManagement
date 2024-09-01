63db45a60e29178af91fa157560e75eb30c6d460
robyafrizal <robyafrizal86@gmail.com>
2024-09-02 06:05:56 +0700

    Register and Login Page complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false)
+      const res = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/') //Direct to homepage
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="max-w-lg mx-auto">
+      <div className="text-3xl text-center font-semibold my-7">Login</div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          className="bg-slate-100 rounded-lg p-3"
+          placeholder="Email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          className="bg-slate-100 rounded-lg p-3"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Login"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-3">
+        <p>Don't have an account?</p>
+        <Link to="/register">
+          <span className="text-blue-500">Register</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-3">{error && "Something went wrong"}</p>
+    </div>
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -30,6 +31,7 @@ export default function Register() {
         setError(true);
         return;
       }
+      navigate('/login')
     } catch (error) {
       setLoading(false);
       setError(true);
